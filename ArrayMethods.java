b17c4f26e6759d816fb091bb037e3b8503aa48f8
Evan D <evan.delasota@gmail.com>
2022-02-19 23:25:17 +0000

    Max Sum Subarray of size K

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -196,4 +196,22 @@ class ArrayMethods {
   // 1.8 Zero Matrix
 
   // 1.9 String Rotation
+
+  // Maximum Sum Subarray of Size K
+  public static double[] findSubarrayAverages(int[] arr, int k) {
+    double[] result = new double[arr.length - k + 1];
+    double windowSum = 0;
+    int windowStart = 0;
+    for (int windowEnd = 0; windowEnd < arr.length; ++windowEnd) {
+      windowSum += arr[windowEnd];
+
+      if (windowEnd >= k - 1) {
+        result[windowStart] = windowSum / k;
+        windowSum -= arr[windowStart];
+        windowStart++;
+      }
+    }
+      
+    return result;
+  }
 }
